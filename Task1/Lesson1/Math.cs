ee7bb4162c3994d7b9b4341e558844e8125f2b92
PyroFinger <egoraborodin@gmail.com>
2026-09-09 11:05:30 +0500

    First task
    Encapsulation + Inheritance + Polymorphism

---
+++ Task1/Lesson1/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Lesson1
+{
+    public class Math
+    {
+        private int Id = 123;
+        internal string name;
+        protected double Sallary;
+
+        public int sum(int num1, int num2) {
+            return num1 + num2;
+            }
+        public int sum(double num1, int num2)
+        {
+                return Convert.ToInt32(num1) + num2;
+        }
+
+            public int sum(double num1, int num2, int num3)
+            {
+                return Convert.ToInt32(num1) + num2 + num3;
+            }
+    }
+}
---
+++ Task1/Lesson2/HMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2
+{
+    internal class HMath:Lesson1.MMath
+    {
+        void test()
+        {
+            sum(3, 7);
+        }
+    }
+}
