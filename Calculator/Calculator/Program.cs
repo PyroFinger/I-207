b4fac39f7c092182a4399ec0ff6f56ff1c10ff90
PyroFinger <egoraborodin@gmail.com>
2026-09-09 12:32:54 +0500

    Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter number 1:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number 2:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Choose *,/,+,-");
+string  sign = Console.ReadLine();
+double result =0;
+if (sign == "*") {
+  result =  num1* num2;
+}
+
+if (sign == "/")
+{
+    result = num1/num2;
+}
+
+if (sign == "+")
+{
+    result = num1+num2;
+}
+
+if (sign == "-")
+{
+    result = num1-num2;
+}
+
+Console.WriteLine(result);
+
+
+
