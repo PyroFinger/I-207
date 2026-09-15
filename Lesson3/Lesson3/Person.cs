2e8d2d4e3d4324ab98ca1b8343083fc128886368
PyroFinger <egoraborodin@gmail.com>
2026-09-15 12:58:14 +0500

    Lesson3

---
+++ Lesson3/Lesson3/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson3
+{
+    internal class Person
+    {
+        public string name = "Egor";
+        public int age = 19;
+        public DateTime birthday = DateTime.Parse("31.08.2007");
+        public bool gender ;
+    }
+}
---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lesson3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Person person = new Person();
+
+            Console.WriteLine(person.name);
+            Console.WriteLine(person.age);
+            Console.WriteLine(person.birthday.ToString("dd.MM.yyyy"));
+            Console.WriteLine((person.gender==true?"Female":"Male"));
+        }
+    }
+}
